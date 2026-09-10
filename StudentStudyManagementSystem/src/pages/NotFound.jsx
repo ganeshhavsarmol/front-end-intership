f999ba57fb9320006626f428fec43ecf59741f84
ganesh <avsarmolganesh93@gmail.com>
2026-09-10 18:22:18 +0530

    student data

--- StudentStudyManagementSystem/src/pages/NotFound.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-export default function NotFound() {
-  return <div className="container page not-found"><h1>404</h1><h2>Page not found</h2><Link className="primary-button" to="/">Go to Home</Link></div>;
-}
